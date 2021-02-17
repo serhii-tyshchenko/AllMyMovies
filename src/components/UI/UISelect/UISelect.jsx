de8893fdfbb462d5b827d6a58483b228cca54af7
Serhii Tyshchenko <serhii.tyshchenko84@gmail.com>
2021-02-17 22:26:44 +0200

    MML-002 | Edited UISelect

--- src/components/UI/UISelect/UISelect.jsx
+++ src/components/UI/UISelect/UISelect.jsx
@@ -1,16 +1,28 @@
 import PropTypes from 'prop-types';
+
 import './UISelect.scss';
 
 const UISelect = (props) => {
   const {
-    value, onChange, options, extraClassName, title,
+    value, onChange, options, extraClassName, title, required,
   } = props;
   const className = extraClassName ? `ui-select ${extraClassName}` : 'ui-select';
 
   return (
-    <select onChange={onChange} value={value} className={className} title={title}>
+    <select
+      onChange={onChange}
+      value={value}
+      required={required}
+      className={className}
+      title={title}
+    >
       {options.map((option) => (
-        <option key={option.value} value={option.value} className="ui-select__option">
+        <option
+          key={option.value}
+          value={option.value}
+          disabled={!option.value}
+          className="ui-select__option"
+        >
           {option.label}
         </option>
       ))}
@@ -22,12 +34,13 @@ UISelect.defaultProps = {
   extraClassName: '',
   onChange: null,
   value: '',
-  title: '',
+  title: 'Select me',
   options: [
     { value: 'opt1', label: 'Option 1' },
     { value: 'opt2', label: 'Option 2' },
     { value: 'opt3', label: 'Option 3' },
   ],
+  required: false,
 };
 
 UISelect.propTypes = {
@@ -41,6 +54,7 @@ UISelect.propTypes = {
       label: PropTypes.string,
     }),
   ),
+  required: PropTypes.bool,
 };
 
 export { UISelect };
