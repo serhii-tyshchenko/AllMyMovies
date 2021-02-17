19cf2ce357a33c855239c3d8ad5e8d6bca1870dc
Serhii Tyshchenko <serhii.tyshchenko84@gmail.com>
2021-02-17 22:25:29 +0200

    MML-002 | Added MovieSorter component

--- src/components/Movie/index.js
+++ src/components/Movie/index.js
@@ -1,5 +1,6 @@
 export { SearchMovieForm } from './SearchMovieForm';
 export { MovieList } from './MovieList';
 export { MovieListItem } from './MovieListItem';
+export { MovieListSorter } from './MovieListSorter';
 export { MovieModal } from './MovieModal';
 export { MovieMenu } from './MovieMenu';
