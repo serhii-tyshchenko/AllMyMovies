45afd4c36e06f13b7f164c1f5e7b74533b642464
Serhii Tyshchenko <serhii.tyshchenko84@gmail.cpm>
2022-07-01 16:23:03 +0300

    edited API status store logic

--- src/store/reducers/_movie-info.js
+++ src/store/reducers/_movie-info.js
@@ -1,23 +1,14 @@
 /* eslint-disable default-param-last */
-import {
-  GET_MOVIE_INFO,
-  API_REQUEST_STARTED,
-  API_REQUEST_ENDED,
-  SIGN_OUT,
-} from '../action-types';
+import { GET_MOVIE_INFO, SIGN_OUT } from '../action-types';
 
-const initialState = { data: {}, isLoading: false };
+const initialState = {};
 
 export const movieInfo = (state = initialState, action) => {
   const { type, payload } = action;
 
   switch (type) {
     case GET_MOVIE_INFO:
-      return { ...state, data: payload };
-    case API_REQUEST_STARTED:
-      return { ...state, isLoading: true };
-    case API_REQUEST_ENDED:
-      return { ...state, isLoading: false };
+      return { ...payload };
     case SIGN_OUT:
       return initialState;
     default:
